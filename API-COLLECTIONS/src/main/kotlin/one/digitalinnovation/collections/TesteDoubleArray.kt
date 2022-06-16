ddabee533d5f0175703f9d0e3febb98037bf7e70
chpm1000 <carloshenriquelaleque@gmail.com>
2022-06-16 00:49:33 -0300

    Create Arrays

---
+++ API-COLLECTIONS/src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.collections
+
+fun main(){
+ val salarios = DoubleArray (size = 3)
+
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    salarios.forEachIndexed { Index, salario ->
+        salarios[Index] = salario * 1.1
+
+    }
+    salarios.forEach { println(1t) }
+    }
